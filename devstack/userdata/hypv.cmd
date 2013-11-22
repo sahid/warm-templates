0a853aafd1d57a0330985b553e6bbda7e2087d10
Sahid Orentino Ferdjaoui <sahid.ferdjaoui@cloudwatt.com>
2013-11-22 11:49:24 +0100

    Review of some configurations

--- devstack/userdata/hypv.cmd
+++ devstack/userdata/hypv.cmd
@@ -5,6 +5,7 @@ cd /home/ubuntu/devstack
 MY_IP=`/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
 MY_PASSWORD=ok
 MY_TOKEN=ok
+MY_CONTROLLER=192.168.42.11
 
 cat > localrc <<EOL
 HOST_IP=$MY_IP
@@ -14,20 +15,22 @@ FIXED_NETWORK_SIZE=4096
 FLOATING_RANGE=192.168.42.128/25
 MULTI_HOST=1
 LOGFILE=/opt/stack/logs/stack.sh.log
+
 ADMIN_PASSWORD=$MY_PASSWORD
 MYSQL_PASSWORD=$MY_PASSWORD
 RABBIT_PASSWORD=$MY_PASSWORD
 SERVICE_PASSWORD=$MY_PASSWORD
 SERVICE_TOKEN=$MY_TOKEN
+
 DATABASE_TYPE=mysql
-SERVICE_HOST=192.168.42.11
-MYSQL_HOST=192.168.42.11
-RABBIT_HOST=192.168.42.11
-GLANCE_HOSTPORT=192.168.42.11:9292
+SERVICE_HOST=$MY_CONTROLLER
+MYSQL_HOST=$MY_CONTROLLER
+RABBIT_HOST=$MY_CONTROLLER
+GLANCE_HOSTPORT=$MY_CONTROLLER:9292
 ENABLED_SERVICES=n-cpu,n-net,n-api,c-sch,c-api,c-vol
 EOL
 
-sleep 500 # needs to wait the controller is running...
+sleep 300 # needs to wait the controller is running...
 
 cd /home/ubuntu/ && chown -R ubuntu:ubuntu devstack/
 cd /home/ubuntu/devstack && sudo -u ubuntu ./stack.sh
