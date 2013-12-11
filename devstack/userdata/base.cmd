f29f7d3b0c9c8f716d9c16c647d782921a51178d
Sahid Orentino Ferdjaoui <sahid.ferdjaoui@cloudwatt.com>
2013-12-11 16:45:28 +0100

    fix ovs

--- devstack/userdata/base.cmd
+++ devstack/userdata/base.cmd
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+
+apt-get install -y --force-yes ubuntu-cloud-keyring
 cat > /etc/apt/sources.list.d/cloudarchive-havana.list <<EOL
 deb http://ubuntu-cloud.archive.canonical.com/ubuntu precise-updates/havana main
 deb-src http://ubuntu-cloud.archive.canonical.com/ubuntu precise-updates/havana main
 EOL
 
 apt-get update
-#apt-get upgrade -y --force-yes
-apt-get install -y git
+apt-get upgrade -y --force-yes
+apt-get install -y --force-yes git
 
 cd /home/ubuntu
 
